87d2c9ea4c4985d42cbf3cf12fde2d25f2d56b37
Thomas Lambert <tplambert@uncg.edu>
2026-03-17 12:21:06 -0400

    added backend API folder with spring initializr setup and created new neon database

---
+++ backend_api/src/test/java/com/sp26_team8/HelpRent/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp26_team8.HelpRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
