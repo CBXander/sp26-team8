3ee05abac664af7f1e4d9787566b9abfa88bfb8f
Thomas Lambert <tplambert@uncg.edu>
2026-03-21 14:32:53 -0400

    added Fixture class

---
+++ backend_api/src/main/java/com/sp26_team8/HelpRent/entity/Fixture.java
@@ -0,0 +1,76 @@
+package com.sp26_team8.HelpRent.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fixture{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long fixtureId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    private List<HelpGuide> helpGuides = new ArrayList<>();
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    //getters
+    public Long getFixtureId(){
+        return this.fixtureId;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public List<HelpGuide> getGuides(){
+        return this.helpGuides;
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return this.createdAt;
+    } 
+
+    public LocalDateTime getUpdatedAt(){
+        return this.updatedAt;
+    }
+
+    //setters
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setHelpGuides(List<HelpGuide> guides){
+        this.helpGuides = guides;
+    }
+    
+}
