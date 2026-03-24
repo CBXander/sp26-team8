b7bd2b34ec23f3337095aeb3acc0220be94b28a3
CBatista <cabatist@uncg.edu>
2026-03-24 10:58:05 -0400

    Fix User, Add more Bruno

--- backend_api/src/main/java/com/sp26_team8/HelpRent/controller/UserController.java
+++ backend_api/src/main/java/com/sp26_team8/HelpRent/controller/UserController.java
@@ -1,45 +1,45 @@
-package com.sp26_team8.HelpRent.controller;
-import java.util.List;
+    package com.sp26_team8.HelpRent.controller;
+    import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
 
-import com.sp26_team8.HelpRent.entity.User;
-import com.sp26_team8.HelpRent.service.UserService;
+    import com.sp26_team8.HelpRent.entity.User;
+    import com.sp26_team8.HelpRent.service.UserService;
 
-@RestController
-@RequestMapping("/api/users")
-public class UserController {
-    private final UserService userService;
+    @RestController
+    @RequestMapping("/api/users")
+    public class UserController {
+        private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+        public UserController(UserService userService) {
+            this.userService = userService;
+        }
+
+        @PostMapping
+        public ResponseEntity<User> createUser(@RequestBody User user) {
+            return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+        }
+
+        @PutMapping("/{userId}")
+        public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody User updatedUser) {
+            return ResponseEntity.ok(userService.updateUser(userId, updatedUser));
+        }
+
+        @GetMapping("/{userId}")
+        public ResponseEntity<User> getUserById(@PathVariable Long userId) {
+            return ResponseEntity.ok(userService.getUserById(userId));
+        }
+
+        @GetMapping
+        public ResponseEntity<List<User>> getAllUsers() {
+            return ResponseEntity.ok(userService.getAllUsers());
+        }
+
+        @DeleteMapping("/{userId}")
+        public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+            userService.deleteUser(userId);
+            return ResponseEntity.noContent().build();
+        }
     }
-
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{userId}")
-    public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody User updatedUser) {
-        return ResponseEntity.ok(userService.updateUser(userId, updatedUser));
-    }
-
-    @GetMapping("/{userId}")
-    public ResponseEntity<User> getUserById(@PathVariable Long userId) {
-        return ResponseEntity.ok(userService.getUserById(userId));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        return ResponseEntity.ok(userService.getAllUsers());
-    }
-
-    @DeleteMapping("/{userId}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
-        userService.deleteUser(userId);
-        return ResponseEntity.noContent().build();
-    }
-}
--- backend_api/src/main/java/com/sp26_team8/HelpRent/entity/User.java
+++ backend_api/src/main/java/com/sp26_team8/HelpRent/entity/User.java
@@ -51,7 +51,9 @@ public class User {
     public void setEmail(String email){
         this.email = email;
     }
-
+    public void setPasswordHash(String passwordHash){
+        this.passwordHash = passwordHash;
+    }
     // password stuff that will be covered in next class
     
     public void setFirstName(String firstName){
