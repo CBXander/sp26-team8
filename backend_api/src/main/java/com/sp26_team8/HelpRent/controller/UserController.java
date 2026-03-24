c66a98700ee013898899c68002e5684a58f381e7
Thomas Lambert <tplambert@uncg.edu>
2026-03-24 04:30:55 -0400

    created all needed controller files, and filled out UnitController and UserController

---
+++ backend_api/src/main/java/com/sp26_team8/HelpRent/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.sp26_team8.HelpRent.controller;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.sp26_team8.HelpRent.entity.User;
+import com.sp26_team8.HelpRent.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // POST /api/users
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+    }
+
+    // PUT /api/users/{userId}
+    @PutMapping("/{userId}")
+    public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody User updatedUser) {
+        return ResponseEntity.ok(userService.updateUser(userId, updatedUser));
+    }
+
+    // GET /api/users/{userId}
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable Long userId) {
+        return ResponseEntity.ok(userService.getUserById(userId));
+    }
+
+    // GET /api/users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    // DELETE /api/users/{userId}
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.noContent().build();
+    }
+}
